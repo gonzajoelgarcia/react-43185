fa17e41240108606b0c4bbeefc799274a7e3530b
Gonzalo García <gonzajoelgarcia.gg@gmail.com>
2023-05-04 01:07:59 -0300

    Segundo Commit

--- src/App.js
+++ src/App.js
@@ -6,30 +6,80 @@ import ItemListContainer from "./components/ItemListContainer/ItemListContainer"
 function App() {
   const contenido = [
     {
-      titulo: "Boca Juniors Parmalat 1996",
-      descripcion: "Camiseta Retro",
+      titulo: "Boca Juniors Titular 2002",
+      descripcion: "$25.000",
       imagen:
-        "https://d22fxaf9t8d39k.cloudfront.net/d6dcc7e687209748caedb283b7fb4587dcebd910918f058bedfc4791d518378a1866.jpeg",
+        "https://mezzalacamisetas.com.ar/wp-content/uploads/2023/02/20230301_173051_0000.png",
     },
     {
-      titulo: "Francia Zidane 2000",
-      descripcion: "Camiseta Retro",
+      titulo: "Francia Titular 2006",
+      descripcion: "$25.000",
       imagen:
-        "https://d22fxaf9t8d39k.cloudfront.net/5959630ac33cbdc2038fbad5f8149d89852da3cf6930431338980f7445de7d311866.jpeg",
+        "https://mezzalacamisetas.com.ar/wp-content/uploads/2023/02/s-l1600-jpg.webp",
+    },
+    {
+      titulo: "Real Madrid Titular 2006/07",
+      descripcion: "$25.000",
+      imagen:
+        "https://mezzalacamisetas.com.ar/wp-content/uploads/2023/02/benzalface_8e8ea97f-bace-4310-9e7b-4f9c1b25a1e5.webp",
+    },
+    {
+      titulo: "Lazio Visitante 2000/01",
+      descripcion: "$25.000",
+      imagen:
+        "https://mezzalacamisetas.com.ar/wp-content/uploads/2023/02/3131-Editado.png",
+    },
+    {
+      titulo: "Manchester United Titular 1992/93",
+      descripcion: "$25.000",
+      imagen:
+        "https://mezzalacamisetas.com.ar/wp-content/uploads/2021/03/united9293.jpg",
+    },
+    {
+      titulo: "Lazio Titular 2022/23",
+      descripcion: "$28.000",
+      imagen:
+        "https://mezzalacamisetas.com.ar/wp-content/uploads/2023/03/descarga-9.webp",
+    },
+    {
+      titulo: "Arsenal Titular 2022/23",
+      descripcion: "$28.000",
+      imagen:
+        "https://mezzalacamisetas.com.ar/wp-content/uploads/2022/09/descarga.jfif",
+    },
+    {
+      titulo: "FC Barcelona Titular 2022/23",
+      descripcion: "$26.500",
+      imagen:
+        "https://mezzalacamisetas.com.ar/wp-content/uploads/2022/09/descarga-5.jfif",
+    },
+    {
+      titulo: "Japón Especial 2023",
+      descripcion: "$32.000",
+      imagen:
+        "https://mezzalacamisetas.com.ar/wp-content/uploads/2023/03/PhotoRoom_004_20230101_164504.png",
+    },
+    {
+      titulo: "Corinthians Tercera 2022/23",
+      descripcion: "$26.500",
+      imagen:
+        "https://mezzalacamisetas.com.ar/wp-content/uploads/2023/01/3275993394-Editado.png",
     },
   ];
   return (
     <div className="App">
       <Navbar navbarLogo={"FUT"} />
-      <ItemListContainer greeting={"Bienvenido a FUT"} />;
-      {contenido.map((camiseta, index) => (
-        <Card
-          key={index}
-          titulo={camiseta.titulo}
-          descripcion={camiseta.descripcion}
-          imagen={camiseta.imagen}
-        />
-      ))}
+      <ItemListContainer greeting={"Bienvenido a FUT"} />
+      <div className="card-container">
+        {contenido.map((camiseta, index) => (
+          <Card
+            key={index}
+            titulo={camiseta.titulo}
+            descripcion={camiseta.descripcion}
+            imagen={camiseta.imagen}
+          />
+        ))}
+      </div>
     </div>
   );
 }
--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,19 +1,28 @@
 import React from "react";
+import "./CardList.css";
 
 const Card = (props) => {
   const { imagen, titulo, descripcion } = props;
 
   return (
-    <div className="card" style={{ width: "18rem" }}>
-      <img src={imagen} className="card-img-top" alt=""></img>
-      <div className="card-body">
-        <h5 className="card-title">{titulo}</h5>
-        <p className="card-text">{descripcion}</p>
-        <button href="#" className="btn btn-primary">
-          AGREGAR
-        </button>
-      </div>
-    </div>
+    <html>
+      <body>
+        <div className="card" style={{ width: "18rem" }}>
+          <div class="contenedor">
+            <div class="imagen">
+              <img src={imagen} className="card-img-top" alt=""></img>
+            </div>
+            <div className="card-body">
+              <h5 className="card-title">{titulo}</h5>
+              <p className="card-text">{descripcion}</p>
+              <button href="#" className="btn btn-primary">
+                AGREGAR
+              </button>
+            </div>
+          </div>
+        </div>
+      </body>
+    </html>
   );
 };
 
