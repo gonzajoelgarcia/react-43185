fa989300014ee69862236b7eefdd65a712b3548a
Gonzalo García <gonzajoelgarcia.gg@gmail.com>
2023-05-24 21:47:42 -0300

    FIREBASE

---
+++ src/firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDhVyHqQM9R1fOQw-I74I4oglpMBvaB5Ag",
+  authDomain: "proyecto-fut.firebaseapp.com",
+  projectId: "proyecto-fut",
+  storageBucket: "proyecto-fut.appspot.com",
+  messagingSenderId: "255413906795",
+  appId: "1:255413906795:web:6ba6c8084051e4558f302e",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
